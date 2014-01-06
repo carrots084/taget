3e980d3f6ff13fe01fe1bc888b65ccde406bbee8
Robert Cochran <robert-git@cochranmail.com>
2014-01-05 20:57:29 -0800

    Added forgotten copyright header to pathfinder.lua

--- pathfinder.lua
+++ pathfinder.lua
@@ -1,3 +1,24 @@
+--[[
+
+TAGET - The 'Text Adventure Game Engine Thingy', used for the creation of simple text adventures
+Copyright (C) 2013 Robert Cochran
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License in the LICENSE file for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+]]
+
 local p = {};
 
 local endpoints = {};
