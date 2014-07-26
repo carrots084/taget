7de6755f3ccc7bf7401e0bbe688ad8dbd6864231
Robert Cochran <robert-git@cochranmail.com>
2014-07-26 14:39:56 -0700

    Added more items for testing purposes

--- data/items.lua
+++ data/items.lua
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 return {
 	[0] = {
 		name = "None",
-		type = 0,
+		type = taget.item.type.none,
 		description = [[
 There's nothing here.
 ]],
@@ -57,6 +57,56 @@ A useless ring. Greaaat. :/
 ]],
 		onTurn = function()
 			print("Your useless ring continues to be so!\n");
+		end,
+	},
+	{
+		name = "Worn Helmet",
+		type = taget.item.type.helmet,
+		description = [[
+A really worn down helmet. Not much, but better than nothing...
+]],
+		onHit = function(a) return (a + 1) end,
+	},
+	{
+		name = "Worn Chestplate",
+		type = taget.item.type.chestplate,
+		description = [[
+A really worn down chestplate. Not much, but better than nothing...
+]],
+		onHit = function(a) return (a + 1) end,
+	},
+	{
+		name = "Worn Leggings",
+		type = taget.item.type.leggings,
+		description = [[
+Some really worn down leggings. Not much, but better than nothing...
+]],
+		onHit = function(a) return (a + 1) end,
+	},
+	{
+		name = "Worn Boots",
+		type = taget.item.type.boots,
+		description = [[
+Some really worn down boots. Not much, but better than nothing...
+]],
+		onHit = function(a) return (a + 1) end,
+	},
+	{
+		name = "Bread",
+		type = taget.item.type.food,
+		description = [[
+Just your average loaf of bread.
+]],
+		onUse = function()
+			local p = taget.player;
+
+			if p.health + 3 < p.maxHealth then
+				p.health = p.health + 3;
+			else
+				p.health = p.maxHealth;
+			end
+
+			print("Yum! The bread restored 3 health points!\n");
 		end,
 	},
 };
